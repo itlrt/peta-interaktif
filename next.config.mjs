ba5c16cde6c9cac976fc0dc085f4a726368197cd
Krisnabhakti <krisnabhakti10@gmail.com>
2025-03-17 12:26:48 +0700

    Mengubah konfigurasi Next.js dengan menghapus pengaturan eksperimen dan mengekspor konfigurasi sebagai default untuk mendukung output standalone.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    outputStandalone: true,
-  }
+  output: 'standalone'
 }
 
-module.exports = nextConfig
+export default nextConfig
